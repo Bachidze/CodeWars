b0a64bebd11fb7a34e9a219b9cfb551a967b415c
bachidze <bachidze90@gmail.com>
2024-04-14 17:03:49 +0400

    Feat(Add):Appear users/todos in dom

--- PromisesFetch2Api/main.js
+++ PromisesFetch2Api/main.js
@@ -1,3 +1,5 @@
+const cont = document.querySelector(".container");
+
 const fetchData = () =>
   new Promise((res) => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -12,7 +14,20 @@ const fetchData2 = () =>
       .then((result2) => res2(result2));
   });
 
-Promise.all([fetchData(), fetchData2()]).then(([f1, f2]) => {
-  console.log(f1);
-  console.log(f2);
+Promise.all([fetchData(), fetchData2()]).then(([users, todos]) => {
+  console.log(users);
+  console.log(todos);
+  displayData(users);
 });
+
+function displayData(data) {
+  cont.innerHTML = data.map(
+    (el) => `
+      <div>
+        <h2>ID: ${el.id}</h2>
+        <h2>User ID: ${el.userId}</h2>
+        <h2>Title: ${el.title}</h2> <!-- Assuming there is a 'title' property -->
+      </div>
+    `
+  ).join(''); 
+}
